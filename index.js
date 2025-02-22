d9d853f828fea5076de68b93a6fd5f32dc1c3a91
Viviana Vera Falconí <vivianavera03@gmail.com>
2025-02-22 00:56:21 -0500

    express

--- index.js
+++ index.js
@@ -2,40 +2,13 @@ require('dotenv').config()
 const http = require("http")
 const readline = require("readline");
 const axios = require("axios");
+const app = require("express")
 
 //const
 const PORT = process.env.PORT
-const GEMINI_KEY = process.env.GEMINI_API
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
 
-function chat(){
-    rl.question("Tú", async (mensaje) => {
-        try {
-            body = {
-                "contents": [{
-                  "parts":[{"text": mensaje}]
-                }]
-            }
-            const response = await axios.post(GEMINI_KEY, body)
-        } catch (error) {
-            
-        }
-
-        chat();
-        
-    })
-}
-
-function requestController() {
-    chat();
-    
-}
-
-const server = http.createServer(requestController)
-
-server.listen(PORT)
-
+app.listen(
+    PORT,
+    () => console.log(`its alive in ${PORT}`)
+)
