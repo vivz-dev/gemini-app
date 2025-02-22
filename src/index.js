e000ed716d9ff597ac88ddc11d95fab5633aa2d9
Viviana Vera Falconí <vivianavera03@gmail.com>
2025-02-22 12:53:26 -0500

    routas 2

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ const express = require("express")
 const v1Router = require("./v1/routes/messageRoutes")
 
 const app = express()
+
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json()); // Middleware para parsear JSON
 app.use("/api/v1", v1Router)
 
 app.listen(PORT, ()=>{
