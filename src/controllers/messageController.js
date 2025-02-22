2c052e07074b65ec70a706a841114fcf8d29b332
Viviana Vera Falconí <vivianavera03@gmail.com>
2025-02-22 11:51:34 -0500

    routas 2

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -1,14 +1,14 @@
 const messageService = require("../services/messageService")
 
 const createNewMsg = (req, res) => {
-    // const {body} = req;
+    const {body} = req;
 
     // const newMessage = {
     //     text: body.text
     // }
 
     try {
-        const messageSent = messageService.createMsg(req)
+        const messageSent = messageService.createMsg(body)
         res.status(201).send({ status: "OK", data: messageSent });
     } catch (error) {
         console.log(error)
