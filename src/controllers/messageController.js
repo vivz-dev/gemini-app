dc1eec22a09b82d5aebf08c76b58f3aaac8b3e3e
Viviana Vera Falconí <vivianavera03@gmail.com>
2025-02-22 14:02:26 -0500

    routas 2

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -5,14 +5,14 @@ const createNewMsg = async (req, res) => {
     if (!req.body) {
         return res.status(400).json({ error: "El cuerpo de la solicitud es inválido. Body not found" });
     }
-    const {text} = req.body.text;
+    const {text} = req.body;
     if (!text) {
         return res.status(400).json({ error: "El cuerpo de la solicitud es inválido. Text not found." });
     }
     console.log(`texto: ${text}`)
     try {
         const messageSent = await messageService.createMsg(text, res)
-        res.send({ status: "OK", message: messageSent });
+        res.send(messageSent);
     } catch (error) {
         console.log(error)
     }
--- src/services/messageService.js
+++ src/services/messageService.js
@@ -1,20 +1,16 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-
 const createMsg = async (newMessage, res) => {
-    // console.log(`newmessage -> ${newMessage}`)
     const respuestaGemini = await geminiAPI(newMessage);
     console.log(`respuesta GEMINI ${respuestaGemini}`)
-    // res.send(`enviando peticion a GEMINI ${newMessage}`)
-    return newMessage
+    return respuestaGemini
 }
 
-async function geminiAPI(params) {
+async function geminiAPI(newMessage) {
     const genAI = new GoogleGenerativeAI("AIzaSyAcYcGPtxzQPx_KfNic06QRXBjIfYouTRE");
     const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
-    const prompt = 'holaaaa, como estas?';
-    const result = await model.generateContent(prompt);
-    console.log(result.response.text());
+    const result = await model.generateContent(newMessage);
+    return result.response.text()
 }
 
 module.exports = {
