883aed6516e94e22f757729c8bb8d257172171c8
Viviana Vera Falconí <vivianavera03@gmail.com>
2025-02-22 14:08:19 -0500

    routas 2

---
+++ src/services/geminiService.js
@@ -0,0 +1,15 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+async function geminiAPI(newMessage) {
+    const genAI = new GoogleGenerativeAI("AIzaSyAcYcGPtxzQPx_KfNic06QRXBjIfYouTRE");
+    const model = genAI.getGenerativeModel({
+        model: "gemini-1.5-flash",
+        systemInstruction: "Eres un asistente que habla español. Ayuda al usuario en lo que requiera. Usa lenguaje amigable y pocos emojis.",
+    });
+    const result = await model.generateContent(newMessage);
+    return result.response.text()
+}
+
+module.exports = {
+    geminiAPI
+}
--- src/services/messageService.js
+++ src/services/messageService.js
@@ -1,16 +1,8 @@
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+const geminiService = require("./geminiService")
 
 const createMsg = async (newMessage, res) => {
-    const respuestaGemini = await geminiAPI(newMessage);
-    console.log(`respuesta GEMINI ${respuestaGemini}`)
+    const respuestaGemini = await geminiService.geminiAPI(newMessage);
     return respuestaGemini
-}
-
-async function geminiAPI(newMessage) {
-    const genAI = new GoogleGenerativeAI("AIzaSyAcYcGPtxzQPx_KfNic06QRXBjIfYouTRE");
-    const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
-    const result = await model.generateContent(newMessage);
-    return result.response.text()
 }
 
 module.exports = {
