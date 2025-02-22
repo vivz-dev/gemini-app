c15c28111bec8f46879df2b59c603c36153c2f07
Viviana Vera Falconí <vivianavera03@gmail.com>
2025-02-22 12:24:28 -0500

    routas 2

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -1,13 +1,13 @@
 const messageService = require("../services/messageService")
 
-const createNewMsg = (req, res) => {
+const createNewMsg = async (req, res) => {
     const {body} = req;
 
     // const newMessage = {
     //     text: body.text
     // }
     try {
-        const messageSent = messageService.createMsg(body, res)
+        const messageSent = await messageService.createMsg(body, res)
         res.send({ status: "OK", message: messageSent });
     } catch (error) {
         console.log(error)
--- src/services/messageService.js
+++ src/services/messageService.js
@@ -1,6 +1,21 @@
-const createMsg = (newMessage, res) => {
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+
+const createMsg = async (newMessage, res) => {
+    
+    console.log(`newmessage -> ${newMessage}`)
+    const respuestaGemini = await geminiAPI(newMessage);
+    console.log(`respuesta GEMINI ${respuestaGemini}`)
     // res.send(`enviando peticion a GEMINI ${newMessage}`)
     return newMessage
+}
+
+async function geminiAPI(params) {
+    const genAI = new GoogleGenerativeAI("AIzaSyAcYcGPtxzQPx_KfNic06QRXBjIfYouTRE");
+    const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+    const prompt = 'holaaaa, como estas?';
+    const result = await model.generateContent(prompt);
+    console.log(result.response.text());
 }
 
 module.exports = {
