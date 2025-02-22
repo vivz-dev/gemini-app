ea61b295e57c778de27deb5a3d9b79a17bd4c3e1
Viviana Vera Falconí <vivianavera03@gmail.com>
2025-02-22 00:44:25 -0500

    chatbot

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 require('dotenv').config()
 const http = require("http")
+const readline = require("readline");
+const axios = require("axios");
 
-function requestController(){
-    console.log("Hola mundo")
+//const
+const PORT = process.env.PORT
+const GEMINI_KEY = process.env.GEMINI_API
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function chat(){
+    rl.question("Tú", async (mensaje) => {
+        try {
+            body = {
+                "contents": [{
+                  "parts":[{"text": mensaje}]
+                }]
+            }
+            const response = await axios.post(GEMINI_KEY, body)
+        } catch (error) {
+            
+        }
+
+        chat();
+        
+    })
+}
+
+function requestController() {
+    chat();
+    
 }
 
 const server = http.createServer(requestController)
 
-server.listen(process.env.PORT, function () {
-    console.log("App corriendo en ", process.env.PORT)
-    
-})
-//c
+server.listen(PORT)
 
