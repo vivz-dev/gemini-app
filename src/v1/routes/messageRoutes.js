a3e77de70e30504043995228f2433138f354857a
Viviana Vera Falconí <vivianavera03@gmail.com>
2025-02-22 23:49:18 -0500

    limitar solo a los webhooks de meta

--- src/v1/routes/messageRoutes.js
+++ src/v1/routes/messageRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const messageController = require("../../controllers/messageController")
 
 router
-    .post('/',messageController.createNewMsg)
-    .post('/ws/',messageController.sendWhatsappMessage)
+    // .post('/',messageController.createNewMsg)
+    // .post('/ws/',messageController.sendWhatsappMessage)
     .post('/webhook', messageController.webhookListener)
     .get('/webhook', messageController.webHookReceiver)
 
